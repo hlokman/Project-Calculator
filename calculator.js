b731cdd4c759ea02a4464dbec3cdc58ef2492116
hlokman <abdallahlokmanhenni@gmail.com>
2023-05-23 01:13:55 +0200

    Create 4 functions for basics math operators

---
+++ calculator.js
@@ -0,0 +1,13 @@
+//Functions for add, subtract, multiply and divide
+function add(a,b) {
+    return a + b;
+};
+function subtract(a,b) {
+    return a - b;
+};
+function multiply(a,b) {
+    return a * b;
+};
+function divide(a,b) {
+    return a / b;
+};
